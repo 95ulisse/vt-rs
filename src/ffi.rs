49eb98c17a840b80ec053e66b19968db0e17db47
Marco Cameriero <me@marcocameriero.net>
2019-09-03 16:57:34 +0200

    FFI layer.

---
+++ src/ffi.rs
@@ -0,0 +1,70 @@
+use std::io;
+use std::os::unix::io::RawFd;
+use nix::libc::*;
+
+// Some constants missing from `libc`
+const VT_OPENQRY: c_int = 0x5600;
+const VT_GETSTATE: c_int = 0x5603;
+const VT_ACTIVATE: c_int = 0x5606;
+const VT_WAITACTIVE: c_int = 0x5607;
+const VT_DISALLOCATE: c_int = 0x5608;
+const VT_LOCKSWITCH: c_int = 0x560B;
+const VT_UNLOCKSWITCH: c_int = 0x560C;
+
+// Structures for the vt ioctls
+#[repr(C)]
+pub struct VtStat {
+	pub v_active: c_ushort,
+	pub v_signal: c_ushort,
+	pub v_state: c_ushort
+}
+
+macro_rules! ioctl_get_wrapper {
+    ($fname:ident, $code:ident, $t:ty) => {
+        #[inline]
+        pub fn $fname(fd: RawFd) -> io::Result<$t> {
+            unsafe {
+                let mut data: $t = ::std::mem::uninitialized();
+                let res = loop {
+                    let res = ioctl(fd, $code as _, &mut data);
+                    if res != EINTR {
+                        break res;
+                    }
+                };
+                match res {
+                    -1 => Err(io::Error::from_raw_os_error(res)),
+                    _ => Ok(data)
+                }
+            }
+        }
+    };
+}
+
+macro_rules! ioctl_set_wrapper {
+    ($fname:ident, $code:ident, $t:ty) => {
+        #[inline]
+        pub fn $fname(fd: RawFd, arg: $t) -> io::Result<()> {
+            unsafe {
+                let res = loop {
+                    let res = ioctl(fd, $code as _, arg);
+                    if res != EINTR {
+                        break res;
+                    }
+                };
+                match res {
+                    -1 => Err(io::Error::from_raw_os_error(res)),
+                    _ => Ok(())
+                }
+            }
+        }
+    };
+}
+
+// Ioctl function wrappers
+ioctl_get_wrapper!(vt_openqry, VT_OPENQRY, c_int);
+ioctl_get_wrapper!(vt_getstate, VT_GETSTATE, VtStat);
+ioctl_set_wrapper!(vt_activate, VT_ACTIVATE, c_int);
+ioctl_set_wrapper!(vt_waitactive, VT_WAITACTIVE, c_int);
+ioctl_set_wrapper!(vt_disallocate, VT_DISALLOCATE, c_int);
+ioctl_set_wrapper!(vt_lockswitch, VT_LOCKSWITCH, c_int);
+ioctl_set_wrapper!(vt_unlockswitch, VT_UNLOCKSWITCH, c_int);
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use std::io::{self, Write, Read};
 use std::fs::{File, OpenOptions};
+use std::os::unix::io::AsRawFd;
+use nix::libc::*;
 use nix::sys::termios::Termios;
+
+mod ffi;
 
 pub struct Console {
     file: File
@@ -17,23 +21,25 @@ impl Console {
     }
 
     pub fn get_current_vt(&self) -> io::Result<Vt>{
-        
+        let vtstate = ffi::vt_getstate(self.file.as_raw_fd())?;
+        Ok(Vt::with_number(self, vtstate.v_active))
     }
 
     pub fn new_vt(&self) -> io::Result<Vt> {
-
+        unimplemented!()
     }
 
-    pub fn new_vt_with_minimum_number(&self, min: u32) -> io::Result<Vt> {
-
+    pub fn new_vt_with_minimum_number(&self, min: u16) -> io::Result<Vt> {
+        unimplemented!()
     }
 
-    pub fn switch_to(&self, vt_number: u32) -> io::Result<()> {
-
+    pub fn switch_to(&self, vt_number: u16) -> io::Result<()> {
+        ffi::vt_activate(self.file.as_raw_fd(), c_int::from(vt_number))?;
+        ffi::vt_waitactive(self.file.as_raw_fd(), c_int::from(vt_number))
     }
 
     pub fn lock_switch(&self, lock: bool) -> io::Result<()> {
-
+        unimplemented!()
     }
 
 }
@@ -51,14 +57,14 @@ pub enum VtMode {
 
 pub struct Vt<'a> {
     console: &'a Console,
-    number: u32,
+    number: u16,
     file: Option<File>,
     termios: Option<Termios>
 }
 
 impl<'a> Vt<'a> {
     
-    fn with_number(console: &'a Console, number: u32) -> Vt<'a> {
+    fn with_number(console: &'a Console, number: u16) -> Vt<'a> {
         Vt {
             console,
             number,
@@ -67,36 +73,40 @@ impl<'a> Vt<'a> {
         }
     }
 
-    pub fn switch(&self) -> io::Result<()> {
+    pub fn number(&self) -> u16 {
+        self.number
+    }
 
+    pub fn switch(&self) -> io::Result<()> {
+        unimplemented!()
     }
 
     pub fn clear(&self) -> io::Result<()> {
-
+        unimplemented!()
     }
 
     pub fn blank(&self, blank: bool) -> io::Result<()> {
-
+        unimplemented!()
     }
 
     pub fn echo(&self, echo: bool) -> io::Result<()> {
-
+        unimplemented!()
     }
 
     pub fn signals(&self, signals: VtSignals) -> io::Result<()> {
-
+        unimplemented!()
     }
 
     pub fn mode(&self, mode: VtMode) -> io::Result<()> {
-        
+        unimplemented!()
     }
 
 }
 
-impl<'a> Read for Vt<'a> {
+/*impl<'a> Read for Vt<'a> {
 
 }
 
 impl<'a> Write for Vt<'a> {
 
-}
+}*/
